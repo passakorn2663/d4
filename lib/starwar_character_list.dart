d22151f567c537d5e39d2afa57dbdd99ac038b1d
passakorn2663 <passakornbuakeaw@gmail.com>
2021-07-12 19:15:45 +0700

    create starwarcharacretlist

---
+++ lib/starwar_character_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StarWarCharacterList extends StatefulWidget {
+  StarWarCharacterList({Key? key}) : super(key: key);
+
+  @override
+  _StarWarCharacterList createState() {
+    return _StarWarCharacterList();
+  }
+}
+
+class _StarWarCharacterList extends State {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
