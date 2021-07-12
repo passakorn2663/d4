a6d188371f79b12c36344e4308ce9aaceec3852c
passakorn2663 <passakornbuakeaw@gmail.com>
2021-07-12 19:31:02 +0700

    create starwarRepo

---
+++ lib/starwar_repo.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+class People {
+  final String name;
+  People(this.name);
+
+  factory People.fromJson(dynamic data) {
+    return People(data['name']);
+  }
+}
+
+class StarWarRepo {
+  Future<List<People>> fetchPeople({int page = 1}) async {
+    var response = await Dio().get('https://swapi.dev/api/people/?page=$page');
+    List<dynamic> results = response.data['results'];
+    return results.map((p) => People.fromJson(p)).toList();
+  }
+}
