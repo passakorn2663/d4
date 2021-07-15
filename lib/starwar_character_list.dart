7813f517ccab5bdd39a0b020ac6ae4737a4e777c
passakorn2663 <passakornbuakeaw@gmail.com>
2021-07-15 14:23:24 +0700

    cont

--- lib/starwar_character_list.dart
+++ lib/starwar_character_list.dart
@@ -18,9 +18,10 @@ class _StarWarCharacterList extends State<StarWarCharacterList> {
   late int _page;
   late List<People> _people;
   // late bool _hasMore;
-  // late bool _loading;
+  late bool _loading;
   // final int _defaultCharatorPerPageCount = 10;
   // final int _nextPageThreshold = 10;
+  late bool _error;
   final ScrollController _scrollController;
 
   _StarWarCharacterList()
@@ -33,7 +34,8 @@ class _StarWarCharacterList extends State<StarWarCharacterList> {
     _page = 1;
     _people = [];
     // _hasMore = true;
-    // _loading = true;
+    _loading = true;
+    _error = false;
     fetchPeople();
   }
 
@@ -43,10 +45,17 @@ class _StarWarCharacterList extends State<StarWarCharacterList> {
   }
 
   Future<void> fetchPeople() async {
-    var people = await _repo.fetchPeople(page: _page);
-    setState(() {
-      _people = List<People>.from(people);
-    });
+    try {
+      var people = await _repo.fetchPeople(page: _page);
+      setState(() {
+        _loading = false;
+        _people = List<People>.from(people);
+        _page += 1;
+      });
+    } catch (e) {
+      _loading = false;
+      _error = true;
+    }
   }
 
   // String getIdCharacter(url) {
@@ -56,35 +65,80 @@ class _StarWarCharacterList extends State<StarWarCharacterList> {
 
   Widget getBody() {
     _scrollController.addListener(() {});
-    return ListView.builder(
-        controller: _scrollController,
-        itemCount: _people.length,
-        itemBuilder: (context, index) {
-          if (index == _people.length) {
-            fetchPeople();
-          }
-          if (index == _people.length) {}
-          final People people = _people[index];
-          final int id = _people.length;
-          return Card(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 160,
-                  width: 160,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                    image: NetworkImage(
-                        "https://starwars-visualguide.com/assets/img/characters/${id}.jpg"),
+    if (_people.isEmpty) {
+      if (_loading) {
+        return Center(
+            child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: CircularProgressIndicator(),
+        ));
+      } else if (_error) {
+        return Center(
+            child: InkWell(
+          onTap: () {
+            setState(() {
+              _loading = true;
+              _error = false;
+              fetchPeople();
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text("Error while loading photos, tap to try agin"),
+          ),
+        ));
+      }
+    } else {
+      return ListView.builder(
+          controller: _scrollController,
+          itemCount: _people.length,
+          itemBuilder: (context, index) {
+            if (index == _people.length) {
+              if (_error) {
+                return Center(
+                    child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      _loading = true;
+                      _error = false;
+                      fetchPeople();
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text("Error while loading data, tap to try agin"),
+                  ),
+                ));
+              } else {
+                return Center(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: CircularProgressIndicator(),
+                ));
+              }
+            }
+            final People people = _people[index];
+            final int id = _people.length;
+            return Card(
+              child: Column(
+                children: <Widget>[
+                  Image.network(
+                    "https://starwars-visualguide.com/assets/img/characters/${id}.jpg",
                     fit: BoxFit.fitWidth,
-                  )),
-                ),
-                Text(
-                  people.name,
-                ),
-              ],
-            ),
-          );
-        });
+                    width: double.infinity,
+                    height: 160,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(people.name,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16)),
+                  ),
+                ],
+              ),
+            );
+          });
+    }
+    return Container();
   }
 }
